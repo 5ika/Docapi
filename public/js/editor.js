3242269099fef5e6339c39fa2274048d46b49b0f
Sika <timothy.izzo@master.hes-so.ch>
2015-10-25 14:59:33 +0100

    Raccourcis clavier - Optimisation des couleurs - Raccourcis clavier - Possibilité de faire des tabulations - Amélioration du Preview

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,5 +1,6 @@
 var toast = Materialize.toast;
 var preview = true;
+var lastContent = "";
 
 $(document).ready(function() {
     $('.modal-trigger').leanModal();
@@ -26,28 +27,31 @@ function toMarkdown() {
 };
 
 function send() {
-    console.log("Sauvegarde");
-    if ($('#title').val() != "") {
-        var document = {
-            title: $('#title').val(),
-            content: $('#content').val(),
-        };
-        if (id != 0) {
-            document._id = id;
-            $('.link-' + id).text(document.title);
-        }
-        $.post("/api", document, function(data) {
-            if (data && !data.hasOwnProperty('error')) {
-                id = data.id;
-                toast(data.message, 2000);
-                $("#last-save").text("Dernière sauvegarde : " + Date());
-                if (data.redirectToEdit) window.location.href =
-                    '/' + id;
-            } else toast(
-                "Problème serveur. Document non sauvé.",
-                2000);
-        });
-    } else toast("Pas de titre. Document non sauvé", 2000);
+    if ($('#content').val() != lastContent)
+        if ($('#title').val() != "") {
+            console.log("Sauvegarde");
+            var document = {
+                title: $('#title').val(),
+                content: $('#content').val(),
+            };
+            if (id != 0) {
+                document._id = id;
+                $('.link-' + id).text(document.title);
+            }
+            $.post("/api", document, function(data) {
+                if (data && !data.hasOwnProperty('error')) {
+                    id = data.id;
+                    toast(data.message, 2000);
+                    $("#last-save").text("Dernière sauvegarde : " +
+                        Date());
+                    lastContent = document.content;
+                    if (data.redirectToEdit) window.location.href =
+                        '/' + id;
+                } else toast(
+                    "Problème serveur. Document non sauvé.",
+                    2000);
+            });
+        } else toast("Pas de titre. Document non sauvé", 2000);
 };
 
 function del() {
@@ -76,3 +80,35 @@ function togglePreview() {
     }
     preview = !preview;
 }
+
+//Gestion des touches claviers spéciales
+$("textarea").keydown(function(e) {
+    // Tabulation dans l'éditeur
+    if (e.keyCode === 9) {
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+        var $this = $(this);
+        var value = $this.val();
+        $this.val(value.substring(0, start) + "\t" + value.substring(
+            end));
+        this.selectionStart = this.selectionEnd = start + 1;
+        e.preventDefault();
+    }
+});
+$(document).keydown(function(e) {
+    console.log(e.keyCode);
+    if (e.ctrlKey) {
+        switch (e.keyCode) {
+            //CTRL+S : Sauvegarde manuelle
+            case 83:
+                send();
+                e.preventDefault();
+                break;
+                //CTRL+P : Toggle Preview
+            case 80:
+                togglePreview();
+                e.preventDefault();
+                break;
+        }
+    }
+})
